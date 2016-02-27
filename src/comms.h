d9c0fd63b2ab06943dc41960268c92f4a26b00b3
Jeff Turley <pythe314@gmail.com>
2016-02-26 17:13:23 -0700

    Config on Github Pages

--- src/comms.h
+++
@@ -1,2 +0,0 @@
-void requestMyStories();
-void receivedMyStories();
--- src/paths.h
+++
@@ -1,9 +0,0 @@
-#ifndef PATHS_H
-#define PATHS_H
-
-static const GPathInfo CARD_PATH_INFO = {
-  .num_points = 5,
-  .points = (GPoint []) {{6,5},{40,5},{40,25},{12,25},{6,15}}
-};
-
-#endif // PATHS_H
